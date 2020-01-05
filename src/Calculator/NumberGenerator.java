f9d6ab0fcec3ba7e523aa5756f8841265e5a5021
deadmanproqn <deadman.pro.qn@gmail.com>
2020-01-05 15:10:18 +0700

    Update NumberGenerator.java
    review, rework and some comments

--- src/Calculator/NumberGenerator.java
+++ src/Calculator/NumberGenerator.java
@@ -10,19 +10,19 @@ public class NumberGenerator {
     public String questionToDisplay;
 
     public NumberGenerator(){
-        QuestionType();
+        QuestionType(); //Move logic out since you only use this method once
+        
+        firstNumber = GenerateRandomNumber();
+        secondNumber = GenerateRandomNumber();
 
+        while (questionType == 4 && (secondNumber == 0 || firstNumber < secondNumber){
+            //reset everything
             firstNumber = GenerateRandomNumber();
             secondNumber = GenerateRandomNumber();
-
-            while (questionType == 4 && secondNumber == 0){
-                secondNumber = GenerateRandomNumber();
-            }
-            while (questionType == 4 && firstNumber<secondNumber){
-                firstNumber = GenerateRandomNumber();
-            }
-
+        }
+        //??? 
         System.out.println(questionType);
+        //Maybe create an enum for arimethic operation for easier reading? Like:  case Operation.ADD:
         switch (questionType){
             case 1:
                 result = firstNumber + secondNumber;
@@ -42,12 +42,13 @@ public class NumberGenerator {
     public int GenerateRandomNumber(){
         int randomGeneratedNumber;
         Random generateRandomNumber = new Random();
-
+        
         randomGeneratedNumber = generateRandomNumber.nextInt(99);
 
         return randomGeneratedNumber;
     }
     public String GetDisplayText(){
+        //Maybe create an enum to shorten this part to questionToDisplay =  firstNumber + questionTypeEnum.Value + secondNumber;
         switch (questionType){
             case 1:
                 questionToDisplay =  firstNumber + " + " + secondNumber;
@@ -73,6 +74,7 @@ public class NumberGenerator {
     }
 
     public int QuestionType(){
+        //move all logic to parent since it is only used once
         Random questionID = new Random();
         while (questionType==0) {
             questionType = questionID.nextInt(4) + 1;
