233a6e64dbc7b61f6514b8881121a315cb4539f2
Long2511 <53852490@users.noreply.github.com>
2020-01-05 21:10:11 +0700

    test cases added

--- src/Test/NumberGeneratorTest.java
+++ src/Test/NumberGeneratorTest.java
@@ -1,7 +1,6 @@
 package Test;
 
-import Calculator.AddOperation;
-import Calculator.NumberGenerator;
+import Calculator.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,16 +9,29 @@ class NumberGeneratorTest {
 
 
     @org.junit.jupiter.api.Test
-    void verifyTheResult() {
-        generator = new AddOperation(1,1);
-        assertTrue(generator.VerifyTheResult(15));
-        assertTrue(generator.VerifyTheResult(3));
-        assertTrue(generator.VerifyTheResult(54));
-        assertTrue(generator.VerifyTheResult(1));
+    void verifyTheResultAdd() {
+        generator = new AddOperation(3,4);
+        assertTrue(generator.VerifyTheResult(7));
+    }
+    @org.junit.jupiter.api.Test
+    void verifyTheResultSubtract() {
+        generator = new SubtractOperation(8,4);
+        assertTrue(generator.VerifyTheResult(4));
+    }
+    @org.junit.jupiter.api.Test
+    void verifyTheResultMultiple() {
+        generator = new MultipleOperation(8,4);
+        assertTrue(generator.VerifyTheResult(32));
+    }
+    @org.junit.jupiter.api.Test
+    void verifyTheResultDivide() {
+        generator = new DivideOperation(8,4);
+        assertTrue(generator.VerifyTheResult(2));
     }
 
     @org.junit.jupiter.api.Test
-    void calculation() {
-        assertEquals(2, generator.Calculation());
+    void calculationAdd() {
+        generator = new AddOperation(2,3);
+        assertEquals(5, generator.Calculation());
     }
 }
